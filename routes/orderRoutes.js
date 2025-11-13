686f7a5c757f957de3d45c8a5d8949419a78227b
RIDONE HOSSAIN MOON <168392332+RidOneMoon@users.noreply.github.com>
2025-11-13 21:23:07 +0800

    i added '/my-orders path

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -8,6 +8,6 @@ const ordersController = require('../controllers/ordersController');
 // Pri
 router.post('/', ordersController.addOrder); 
 router.post('/batch',ordersController.addOrders)
- 
+router.get('/my-orders', ordersController.getMyOrders); 
 
 module.exports = router;
